8a02d71fb3a58333a4816dc34b259845286d292f
Samuel Hammer <ham14447@spengergasse.at>
2014-12-15 22:21:19 +0100

    Moved all classes into Haupt.java file and renamed it to TestDataGenerator.java

--- src/at/hammer/testdatagenerator/TestDataGenerator.java
+++ src/at/hammer/testdatagenerator/TestDataGenerator.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 import java.util.LinkedList;
 import java.io.*;
 
-public class Haupt {
+import at.hammer.testdatagenerator.RandomStringGenerator.Mode;
+
+public class TestDataGenerator {
 
     public static void main(String[] args) {
         if(args.length != 0) {
@@ -111,5 +113,143 @@ public class Haupt {
      */
     public static int randBetween(int start, int end) {
         return start + (int) Math.round(Math.random() * (end - start));
+    }
+}
+
+
+
+/**
+ * Created by Samuel Hammer on 30.09.2014.
+ */
+class Titles {
+    private String titleId;
+    private String name;
+    private int duration;
+    private String artist;
+    private String album;
+    private LocalDate release;
+    private int playedCount;
+	private int chartPosition;
+
+    public Titles(String titleId, String name, int duration, String artist, String album, 
+    		LocalDate release, int playedCount, int chartPosition) {
+        super();
+        this.titleId = titleId;
+        this.name = name;
+        this.duration = duration;
+        this.artist = artist;
+        this.album = album;
+        this.release = release;
+        this.playedCount = playedCount;
+        this.chartPosition = chartPosition;
+    }
+
+    @Override
+    public String toString() {
+        String t = titleId + "\t" + name + "\t" + duration + "\t" + artist + "\t" + album + "\t" + 
+        		release + "\t" + playedCount + "\t" + chartPosition;
+        return t;
+    }
+
+    public String getTitleId() {
+        return titleId;
+    }
+
+    public void setTitleId(String titleId) {
+        this.titleId = titleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public LocalDate getRelease() {
+        return release;
+    }
+
+    public void setRelease(LocalDate release) {
+        this.release = release;
+    }
+    
+    public int getPlayedCount() {
+		return playedCount;
+	}
+
+	public void setPlayedCount(int playedCount) {
+		this.playedCount = playedCount;
+	}
+
+	public int getChartPosition() {
+		return chartPosition;
+	}
+
+	public void setChartPosition(int chartPosition) {
+		this.chartPosition = chartPosition;
+	}
+}
+
+
+
+class RandomStringGenerator {
+
+    public static enum Mode {
+        ALPHA, ALPHANUMERIC, NUMERIC
+    }
+
+    public static String generateRandomString(int length, Mode mode) throws Exception {
+
+        StringBuffer buffer = new StringBuffer();
+        String characters = "";
+
+        switch(mode){
+
+            case ALPHA:
+                characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                break;
+
+            case ALPHANUMERIC:
+                characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                break;
+
+            case NUMERIC:
+                characters = "1234567890";
+                break;
+        }
+
+        int charactersLength = characters.length();
+
+        for (int i = 0; i < length; i++) {
+            double index = Math.random() * charactersLength;
+            buffer.append(characters.charAt((int) index));
+        }
+        return buffer.toString();
     }
 }
